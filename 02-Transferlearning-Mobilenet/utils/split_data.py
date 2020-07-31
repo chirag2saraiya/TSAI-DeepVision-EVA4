c9aeed34f8283c975066d82f18561d2d31966e66
chirag2saraiya <chirag2saraiya@gmail.com>
2020-07-31 22:29:25 +0530

    indentation

--- 02-Transferlearning-Mobilenet/utils/split_data.py
+++ 02-Transferlearning-Mobilenet/utils/split_data.py
@@ -1,6 +1,5 @@
 """Data Exploration
 We can do a brief exploration of the available images. There are a total of 100 categories, which I've already split into training (50%), validation (25%), and testing (25%) folders. The data is clean and each class (I use the terms "class" and "category" interchangeably in this notebook) is stored in a separate folder. The architecture of the folders is thus:
-
 /datadir
     /train
             /class_1
@@ -12,7 +11,6 @@ We can do a brief exploration of the available images. There are a total of 100 
             /class_2
             .
             .
-
 This is a standard organization of the data for cnns and makes it simple to associate the correct labels with the images.
 """
 
@@ -26,7 +24,7 @@ except ImportError:
 finally:
     import split_folders
 
-def split_data_folder(input_folder, output_folder, train_ratio =0.8):
+def split_data_folder(input_folder, output_folder, train_ratio=0.8):
     # Split with a ratio.
     # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
     test_ratio = 1.0-train_ratio
