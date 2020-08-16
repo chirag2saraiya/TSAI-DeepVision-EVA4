901bb4c001cbe322bede2ba1791643b2c5383b76
chirag2saraiya <chirag2saraiya@gmail.com>
2020-08-16 07:04:38 -0400

    Static website Commit

---
+++ static_website/scripts/upload.js
@@ -0,0 +1,113 @@
+function uploadAndClassifyImage(){
+  var fileInput = document.getElementById('resnet34FileUpload').files;
+  if (!fileInput.length){
+    return alert('Please choose a file to upload first.');
+  }
+
+  var file = fileInput[0];
+  var filename = file.name
+
+  var formData = new FormData();
+  formData.append(filename, file);
+
+  console.log(filename);
+
+
+$.ajax({
+      async: true,
+      crossDomain: true,
+      method: 'POST',
+      url: 'https://3njah24gii.execute-api.ap-south-1.amazonaws.com/dev/classify',
+      data: formData,
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+})
+.done(function(response){
+  console.log(response);
+  document.getElementById('result').textContent = response;
+})
+.fail(function() {alert ("There was an error while sending prediction request to resnet34 model."); });
+};
+
+
+function mobilenetUploadAndClassifyImage(){
+  var fileInput = document.getElementById('mobileNetFileUpload').files;
+  if (!fileInput.length){
+    return alert('Please choose a file to upload first.');
+  }
+
+  var file = fileInput[0];
+  var filename = file.name
+
+  var formData = new FormData();
+  formData.append(filename, file);
+
+  console.log(filename);
+
+
+$.ajax({
+      async: true,
+      crossDomain: true,
+      method: 'POST',
+      url: 'https://v1agl77crf.execute-api.ap-south-1.amazonaws.com/dev/classify',
+      data: formData,
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+})
+.done(function(response){
+  console.log(response);
+  document.getElementById('resultMobileNet').textContent = response;
+})
+.fail(function() {alert ("There was an error while sending prediction request to resnet34 model."); });
+};
+
+
+
+
+
+
+
+function uploadAndAlignFace(){
+  var fileInput = document.getElementById('faceFileUpload').files;
+  if (!fileInput.length){
+    return alert('Please choose a file to upload first.');
+  }
+
+  var file = fileInput[0];
+  var filename = file.name
+
+  var formData = new FormData();
+  formData.append(filename, file);
+
+  console.log(filename);
+
+
+$.ajax({
+      async: true,
+      crossDomain: true,
+      method: 'POST',
+      url: 'https://noe3b7wagh.execute-api.ap-south-1.amazonaws.com/dev/align',
+      data: formData,
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+})
+.done(function(response){
+  response = JSON.parse(response);
+
+  //console.log(response.img);
+  if(response.result == "True"){
+  $('#faceResult').attr('src', 'data:image/jpeg;base64,'+response.img);
+  }else{
+document.getElementById('errorMsg').textContent = "Please upload Valid Image (Containing only One Face)";
+}
+//$('#faceResult').attr('src', response);
+})
+.fail(function() {alert ("There was an error while sending prediction request to resnet34 model."); });
+};
+
+$('#btnFaceUpload').click(uploadAndAlignFace);
+$('#btnResNetUpload').click(uploadAndClassifyImage);
+$('#btnMobileNetUpload').click(mobilenetUploadAndClassifyImage);
